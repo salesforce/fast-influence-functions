1cda071cae6665cc9dfceacdf68dd9a17c0ac0e5
HanGuo97 <alex.guohan1106@gmail.com>
2020-08-27 23:37:36 -0400

    cluster.cluster.docker.update.sh: migrated to Docker

--- cluster/docker.update.sh
+++ cluster/docker.update.sh
@@ -2,17 +2,28 @@
 set -e
 
 # Constants
-REGISTRY="gcr.io/salesforce-research-internal"
-REPO_BASE="hguo-scratchpad"
+REPO_BASE="fast-influence-functions"
+DIRECTORY_BASE="fast-influence-functions"
+
 
 # Use Git commit SHA as the image tag
 HeadCommitSHA=`git rev-parse --verify HEAD`
 echo ${HeadCommitSHA}
 
-# Using GCloud Builds
-gcloud builds submit \
-    --timeout 1000 \
-    --tag ${REGISTRY}/${REPO_BASE}:${HeadCommitSHA} .
+# Remove, and re-build container
+# docker image rm hanguo97/${REPO_BASE}
+cd /proj/bansallab/users/han/DLResearch
+
+# Using BuiltKit
+# More: https://docs.docker.com/develop/develop-images/build_enhancements/
+DOCKER_BUILDKIT=1 docker build \
+    -t ${REPO_BASE} \
+    -f ${DIRECTORY_BASE}/Dockerfile \
+    --secret id=wandb_apikey,src=secrets/wandb.apikey.secret  .
+
+# Push to the registry
+docker tag ${REPO_BASE} hanguo97/${REPO_BASE}:${HeadCommitSHA}
+docker push hanguo97/${REPO_BASE}:${HeadCommitSHA}
 
 # Modify the tag in the related files
 git checkout -- cluster/docker.run.gpu.sh  # this is important
