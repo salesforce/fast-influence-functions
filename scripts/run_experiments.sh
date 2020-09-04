2cb726054ed3dc243d4d13b60c85fe89482d232a
HanGuo97 <alex.guohan1106@gmail.com>
2020-09-04 18:18:13 -0400

    scripts.run_experiments.py: moved to the main directory

--- scripts/run_experiments.sh
+++ scripts/run_experiments.sh
@@ -5,4 +5,4 @@ export REMOTE_BASE_DIR="/data/home/guest/Experiments/20200904"
 export WANDB_API_KEY="25a0cba5a031d8071bb6fcd352cdb659ba152a92"
 
 # Run Experiments!
-python ./scripts/run_experiments.py
+python ./run_experiments.py
