557ba241e465a37c31395143fba0605cf0629b90
HanGuo97 <alex.guohan1106@gmail.com>
2020-09-04 17:21:48 -0400

    cluster.docker.update.sh: revert partially back to previous version for compatibility

--- cluster/docker.update.sh
+++ cluster/docker.update.sh
@@ -2,30 +2,18 @@
 set -e
 
 # Constants
-REPO_BASE="fast-influence-functions"
-DIRECTORY_BASE="fast-influence-functions"
-
+REGISTRY="gcr.io/salesforce-research-internal"
+REPO_BASE="hguo-scratchpad"
 
 # Use Git commit SHA as the image tag
 HeadCommitSHA=`git rev-parse --verify HEAD`
 echo ${HeadCommitSHA}
 
-# Remove, and re-build container
-# docker image rm hanguo97/${REPO_BASE}
-cd /proj/bansallab/users/han/DLResearch
+# Using GCloud Builds
+gcloud builds submit \
+    --timeout 1000 \
+    --tag ${REGISTRY}/${REPO_BASE}:${HeadCommitSHA} .
 
-# Using BuiltKit
-# More: https://docs.docker.com/develop/develop-images/build_enhancements/
-DOCKER_BUILDKIT=1 docker build \
-    -t ${REPO_BASE} \
-    -f ${DIRECTORY_BASE}/Dockerfile \
-    --secret id=wandb_apikey,src=secrets/wandb.apikey.secret  .
-
-# Push to the registry
-docker tag ${REPO_BASE} hanguo97/${REPO_BASE}:${HeadCommitSHA}
-docker push hanguo97/${REPO_BASE}:${HeadCommitSHA}
-
-cd ./${DIRECTORY_BASE}
 # Modify the tag in the related files
 git checkout -- cluster/docker.run.sh  # this is important
 sed -i -e 's@IMAGE_TAG@'"$HeadCommitSHA"'@' cluster/docker.run.sh
