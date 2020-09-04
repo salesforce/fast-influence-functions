5733440c8f94f0f78e840b8de4551cacd7aed838
HanGuo97 <alex.guohan1106@gmail.com>
2020-09-04 16:53:14 -0400

    scripts: added `run_experiments.sh`

---
+++ scripts/run_experiments.sh
@@ -0,0 +1,8 @@
+# Setup Remote server directory to save outputs
+export REMOTE_BASE_DIR="/data/home/guest/Experiments/20200904"
+
+# Setup Weight And Bias
+export WANDB_API_KEY="25a0cba5a031d8071bb6fcd352cdb659ba152a92"
+
+# Run Experiments!
+python ./scripts/run_experiments.sh
