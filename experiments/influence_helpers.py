6ffda4a156f6f437af003b99d199514c512e5569
HanGuo97 <alex.guohan1106@gmail.com>
2020-10-24 16:04:33 -0400

    experiments/influence_helpers: updated

--- experiments/influence_helpers.py
+++ experiments/influence_helpers.py
@@ -4,15 +4,18 @@ from typing import List, Dict, Tuple, Optional, Union, Any
 
 from experiments import constants
 from experiments import misc_utils
+from influence_utils import parallel
 from influence_utils import nn_influence_utils
 
 
 def compute_influences_simplified(
         k: int,
         model: torch.nn.Module,
-        test_inputs: Dict[str, torch.Tensor],
-        batch_train_data_loader: torch.utils.data.DataLoader,
-        instance_train_data_loader: torch.utils.data.DataLoader,
+        inputs: Dict[str, torch.Tensor],
+        trained_on_task_name: str,
+        eval_task_name: str,
+        train_dataset: torch.utils.data.DataLoader,
+        use_parallel: bool,
         device_ids: Optional[List[int]] = None,
         precomputed_s_test: Optional[List[torch.FloatTensor]] = None,
 ) -> Tuple[Dict[int, float], List[torch.FloatTensor]]:
@@ -20,11 +23,6 @@ def compute_influences_simplified(
     faiss_index = faiss_utils.FAISSIndex(768, "Flat")
     faiss_index.load(constants.MNLI_FAISS_INDEX_PATH)
     print(f"Loaded FAISS index with {len(faiss_index)} entries")
-
-    test_features = misc_utils.compute_BERT_CLS_feature(model, **test_inputs)
-    test_features = test_features.cpu().detach().numpy()
-    KNN_distances, KNN_indices = faiss_index.search(
-        k=k, queries=test_features)
 
     # Make sure indices are sorted according to distances
     # KNN_distances[(
@@ -42,24 +40,78 @@ def compute_influences_simplified(
         n for n, p in model.named_parameters()
         if not p.requires_grad]
 
-    if device_ids is None:
-        (influences,
-         train_inputs_collections,
-         s_test) = nn_influence_utils.compute_influences(
+    # Other settings are not supported as of now
+    if trained_on_task_name == "mnli-2" and eval_task_name == "mnli-2":
+        s_test_damp = 5e-3
+        s_test_scale = 1e4
+        s_test_num_samples = 1000
+
+    if trained_on_task_name == "hans" and eval_task_name == "hans":
+        s_test_damp = 5e-3
+        s_test_scale = 1e6
+        s_test_num_samples = 2000
+
+    if trained_on_task_name == "mnli-2" and eval_task_name == "hans":
+        s_test_damp = 5e-3
+        s_test_scale = 1e6
+        s_test_num_samples = 1000
+
+    if trained_on_task_name == "hans" and eval_task_name == "mnli-2":
+        s_test_damp = 5e-3
+        s_test_scale = 1e6
+        s_test_num_samples = 2000
+
+    if faiss_index is not None:
+        features = misc_utils.compute_BERT_CLS_feature(model, **inputs)
+        features = features.cpu().detach().numpy()
+        KNN_distances, KNN_indices = faiss_index.search(
+            k=k, queries=features)
+    else:
+        KNN_indices = None
+
+    if not use_parallel:
+        model.cuda()
+        batch_train_data_loader = misc_utils.get_dataloader(
+            train_dataset,
+            batch_size=1,
+            random=True)
+
+        instance_train_data_loader = misc_utils.get_dataloader(
+            train_dataset,
+            batch_size=1,
+            random=False)
+
+        influences, _, _ = nn_influence_utils.compute_influences(
             n_gpu=1,
             device=torch.device("cuda"),
-            model=model,
-            test_inputs=test_inputs,
             batch_train_data_loader=batch_train_data_loader,
             instance_train_data_loader=instance_train_data_loader,
+            model=model,
+            test_inputs=inputs,
             params_filter=params_filter,
             weight_decay=constants.WEIGHT_DECAY,
             weight_decay_ignores=weight_decay_ignores,
-            s_test_scale=1000,
-            s_test_num_samples=300,
-            precomputed_s_test=precomputed_s_test,
-            train_indices_to_include=KNN_indices)
+            s_test_damp=s_test_damp,
+            s_test_scale=s_test_scale,
+            s_test_num_samples=s_test_num_samples,
+            train_indices_to_include=KNN_indices,
+            precomputed_s_test=None)
     else:
-        raise ValueError("Deprecated")
+        influences, _ = parallel.compute_influences_parallel(
+            # Avoid clash with main process
+            device_ids=[0, 1, 2, 3],
+            train_dataset=train_dataset,
+            batch_size=1,
+            model=model,
+            test_inputs=inputs,
+            params_filter=params_filter,
+            weight_decay=constants.WEIGHT_DECAY,
+            weight_decay_ignores=weight_decay_ignores,
+            s_test_damp=s_test_damp,
+            s_test_scale=s_test_scale,
+            s_test_num_samples=s_test_num_samples,
+            train_indices_to_include=KNN_indices,
+            return_s_test=False,
+            debug=False)
 
-    return influences, s_test
+    return influences
