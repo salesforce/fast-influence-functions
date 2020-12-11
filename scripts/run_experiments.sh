7dd05c0b819bb614e565f9e6283d8780130ad166
HanGuo97 <alex.guohan1106@gmail.com>
2020-12-11 11:49:51 -0500

    removed deprecated files

--- scripts/run_experiments.sh
+++
@@ -1,8 +0,0 @@
-# Setup Remote server directory to save outputs
-export REMOTE_BASE_DIR="/data/home/guest/Experiments/20200922"
-
-# Setup Weight And Bias
-export WANDB_API_KEY="25a0cba5a031d8071bb6fcd352cdb659ba152a92"
-
-# Run Experiments!
-python ./run_experiments.py $1
